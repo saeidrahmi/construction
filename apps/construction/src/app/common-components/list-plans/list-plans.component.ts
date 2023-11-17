ed492f40bd1186425e1a3f4b7da19ecfeecb742a
Saeid Khodarahmi <skhodarahmi@corelogic.com>
2023-11-17 11:15:23 -0500

    plans

---
+++ apps/construction/src/app/common-components/list-plans/list-plans.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ToastrService } from 'ngx-toastr';
+import { finalize, tap, catchError, of } from 'rxjs';
+import { PlanComponent } from '../plan/plan.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-list-plans',
+  templateUrl: './list-plans.component.html',
+  styleUrls: ['./list-plans.component.css'],
+  standalone: true,
+  imports: [CommonModule, PlanComponent, RouterModule],
+})
+export class ListPlansComponent {
+  apiService = inject(ApiService);
+  destroyRef = inject(DestroyRef);
+  toastService = inject(ToastrService);
+  listPlans: any;
+
+  constructor() {
+    this.apiService
+      .getAllActivePlans()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+
+        tap((plans) => {
+          this.listPlans = plans;
+        }),
+        catchError((err) => {
+          return of(err);
+        })
+      )
+      .subscribe();
+  }
+}
